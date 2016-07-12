e529b910eada285e0d4444daf0469b196d7d6a56
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-07-12 11:06:01 +0100

    finish monday

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,10 @@ describe Oystercard do
     expect(subject).to receive(:balance)
     subject.balance
   end
+
+context "top up"
+  it { is_expected.to respond_to(:top_up).with(1).arguments }
+  it "should be able to top up the balance" do
+    expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+  end
 end
