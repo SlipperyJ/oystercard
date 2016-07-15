c067e57fda981c9322409fa972db6f905a2c0681
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-07-15 16:40:46 +0100

    add basic journey log class

---
+++ lib/journey_log.rb
@@ -0,0 +1,19 @@
+require_relative 'journey'
+
+class JourneyLog
+
+  attr_reader :journey_class, :instantiated_class
+
+  def initialize(journey_class)
+    @journey_class = journey_class
+  end
+
+  def starting_journey(start_point)
+    @instantiated_class = @journey_class.new(start_point)
+  end
+
+  def ending_journey(end_point)
+    @instantiated_class.finish_journey(end_point)
+  end
+
+end
